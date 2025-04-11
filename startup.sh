9d0693fbf6af804a8bf770257ab1c5372ca285ff
thomasmburke <tburke@bu.edu>
2025-04-10 19:47:23 -0700

    fix: GITHUB_TOKEN exporting

--- startup.sh
+++ startup.sh
@@ -25,5 +25,6 @@ GORELEASER_PREVIOUS_TAG=$(head -1 /tmp/release_tags.txt)
 
 echo "export GORELEASER_CURRENT_TAG=$GORELEASER_CURRENT_TAG" >> /tmp/release_env
 echo "export GORELEASER_PREVIOUS_TAG=$GORELEASER_PREVIOUS_TAG" >> /tmp/release_env
+echo "export GITHUB_TOKEN=$GITHUB_TOKEN" >> /tmp/release_env
 
 cat -n /tmp/release_env
